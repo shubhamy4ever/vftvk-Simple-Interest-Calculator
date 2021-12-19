b0ab64e043686a117f36fcb4e17b91e04a1606d8
Shubham Yadav <sspy2172002@gmail.com>
2021-12-19 19:27:36 +0530

    Completed the assignment

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+  let amount = document.getElementById("amount");
+  let resultdiv = document.getElementById("resultdiv");
+  resultdiv.style.display = "block";
+  let slider = document.getElementById("rate");
+  let years = document.getElementById("years");
+  let finalresult = document.getElementById("result");
+  let res = (amount.value * (slider.value * years.value)) / 100;
+  finalresult.innerHTML = res;
+  let amounthere = document.getElementById("amounthere");
+  let ratehere = document.getElementById("ratehere");
+  let yearshere = document.getElementById("yearhere");
+  amounthere.innerHTML = amount.value;
+  ratehere.innerHTML = slider.value+"%";
+  let currentyear = new Date().getFullYear();
+  let convertedvalue = Number(years.value);
+  yearshere.innerHTML = currentyear + convertedvalue;
 }
-        
+function changed() {
+  let slider = document.getElementById("rate");
+  let result = document.getElementById("range");
+  result.innerHTML = slider.value+"%";
+}
