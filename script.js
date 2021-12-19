3bc9b5311edc3f364b89a0ec8a128f78a1eeeeb1
Shubham Yadav <63555820+shubhamy4ever@users.noreply.github.com>
2021-12-19 19:36:16 +0530

    added alert

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 function compute() {
   let amount = document.getElementById("amount");
   let resultdiv = document.getElementById("resultdiv");
-  resultdiv.style.display = "block";
-  let slider = document.getElementById("rate");
-  let years = document.getElementById("years");
-  let finalresult = document.getElementById("result");
-  let res = (amount.value * (slider.value * years.value)) / 100;
-  finalresult.innerHTML = res;
-  let amounthere = document.getElementById("amounthere");
-  let ratehere = document.getElementById("ratehere");
-  let yearshere = document.getElementById("yearhere");
-  amounthere.innerHTML = amount.value;
-  ratehere.innerHTML = slider.value+"%";
-  let currentyear = new Date().getFullYear();
-  let convertedvalue = Number(years.value);
-  yearshere.innerHTML = currentyear + convertedvalue;
+  if (amount.value <= 0) {
+    alert("please enter a positive number");
+  } else {
+    resultdiv.style.display = "block";
+    let slider = document.getElementById("rate");
+    let years = document.getElementById("years");
+    let finalresult = document.getElementById("result");
+    let res = (amount.value * (slider.value * years.value)) / 100;
+    finalresult.innerHTML = res;
+    let amounthere = document.getElementById("amounthere");
+    let ratehere = document.getElementById("ratehere");
+    let yearshere = document.getElementById("yearhere");
+    amounthere.innerHTML = amount.value;
+    ratehere.innerHTML = slider.value + "%";
+    let currentyear = new Date().getFullYear();
+    let convertedvalue = Number(years.value);
+    yearshere.innerHTML = currentyear + convertedvalue;
+  }
 }
 function changed() {
   let slider = document.getElementById("rate");
   let result = document.getElementById("range");
-  result.innerHTML = slider.value+"%";
+  result.innerHTML = slider.value + "%";
 }
